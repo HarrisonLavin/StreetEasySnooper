cb86fd3024d12e00e49971eba94036cb20c011fa
HarrisonLavin <harrison.lavin@gmail.com>
2016-05-10 22:07:16 -0400

    refines page loading

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,16 +1,9 @@
 $(function(){
 
-    $('input#welcome').on("click", function(event){
-      debugger
-      $('form').html("<img align='top' width=100 src='http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif'>")
-      $('form').html("Loading...")
-      $.get("/outbound")
-      })
-
-    $('input').on("click", function(){
-      $('#loading').append("<img align='middle' width=100 src='http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif'>")
+    $('input').on('click', function(){
+      $('input').css('display', "none")
+      $('div').append("<img align='middle' width=100 src='http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif'>")
     })
-
 
 }) 
 
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,6 +5,7 @@ class ListingsController < ApplicationController
   end
 
   def show
+
   end
 
   def outbound
