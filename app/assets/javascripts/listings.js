fa4831a3a0baa7911aa179e2bbabe62800df4195
HarrisonLavin <harrison.lavin@gmail.com>
2016-05-11 08:30:56 -0400

    adds styling

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,10 +1,6 @@
 $(function(){
-
     $('input').on('click', function(){
       $('input').css('display', "none")
       $('div').append("<img align='middle' width=100 src='http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif'>")
     })
-
 }) 
-
-
--- app/models/Listing.rb
+++ app/models/Listing.rb
@@ -1,13 +1,12 @@
 class Listing
-
-attr_accessor :listing_class, :price, :address, :url, :unit
-  def initialize(input)
-    @listing_class = input[:listing_class]
-    @price= input[:price]
-    @address= input[:address]
-    @url= input[:url]
-    @unit= input[:unit]
-  end
+  attr_accessor :listing_class, :price, :address, :url, :unit
+    def initialize(input)
+      @listing_class = input[:listing_class]
+      @price= input[:price]
+      @address= input[:address]
+      @url= input[:url]
+      @unit= input[:unit]
+    end
 
 end
 
